f76b6c4b621a6b2b55587ca493f0f686d106f3e9
Lawrence Eniola <lawrencekunmilola@gmail.com>
2024-02-25 21:47:45 +0100

    demo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,15 @@
-import RevenueIcon from '../assets/revenues.svg';
+//import RevenueIcon from '../assets/revenues.svg';
 import '../styles/hero.css';
+import Toshokan from '../assets/Toshokan.svg';
 
 const Hero = () => {
     return (
         <div className="hero">
             <div>
-                <p className="title">Publish and earn at the same time</p>
-                <p className="content">Lorem ipsum dolor sit amet consectetur. Gravida mauris at tempor amet augue. Fames nisl integer ut amet nunc tincidunt</p>
+                <p className="title">Welcome to Toshokan - Your Gateway to Endless Learning!</p>
+                <p className="content">Dive into a world of knowledge and discover.  Explore curated collections tailored to your interests. Feed your curiosity, one page at a time</p>
             </div>
-            <img src={RevenueIcon} alt="" />
+            <img className='img' src='https://i.pinimg.com/originals/66/e9/09/66e909cf0f6239f0d67f060484c7647b.png' alt="" />
         </div>
     )
 };
