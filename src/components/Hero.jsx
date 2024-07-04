a4674afd4df7b5450f61ac56cce824a4fab35d45
Lawrence Eniola <lawrencekunmilola@gmail.com>
2024-07-04 15:58:20 +0100

    mascot

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 //import RevenueIcon from '../assets/revenues.svg';
 import '../styles/hero.css';
+import mascot from '../assets/mascot.png'
 import Toshokan from '../assets/Toshokan.svg';
 
 const Hero = () => {
@@ -9,7 +10,7 @@ const Hero = () => {
                 <p className="title">Welcome to Toshokan - Your Gateway to Endless Learning!</p>
                 <p className="content">Dive into a world of knowledge and discover.  Explore curated collections tailored to your interests. Feed your curiosity, one page at a time</p>
             </div>
-            <img className='img' src='https://i.pinimg.com/originals/66/e9/09/66e909cf0f6239f0d67f060484c7647b.png' alt="" />
+            <img className='img' src={mascot} alt="" />
         </div>
     )
 };
